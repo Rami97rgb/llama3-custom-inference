c0fb02fbaeffefcc5d8b38f8bf9ca1ba38378591
Rami97rgb <ben.ayed.rami.2@gmail.com>
2025-02-10 18:32:59 +0100

    edit old build code

--- compile_ and_bind_custom_kernels.py
+++ compile_ and_bind_custom_kernels.py
@@ -5,7 +5,7 @@ os.environ['TORCH_USE_CUDA_DSA'] = "1"
 # suported GPU architectures
 os.environ['TORCH_CUDA_ARCH_LIST'] = "8.0 8.6 8.7 8.9 9.0"
 
-# simple way to load the CUDA kernel as a pyTorch extension
+# simple way to compile CUDA kernels and bind them as a pyTorch extension
 flash_attention_rami_extension = load(
     name='custom_kernels_extension',
     sources=["./custom_kernels/custom_kernels.cu", "./custom_kernels/main.cpp"],
